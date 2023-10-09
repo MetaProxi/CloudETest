22d9582d4029578f01d2a96070a49a4bb882c63f
MetaProx <proxidius@gmail.com>
2023-10-09 00:59:08 -0600

    Set up central rodux data singletons

---
+++ src/client/Controllers/DataController.lua
@@ -0,0 +1,58 @@
+--Rodux Data Controller
+
+
+--API Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--Knit
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+--Dependencies
+local Rodux = require(ReplicatedStorage.Packages.Rodux)
+
+--Controller
+local DataController = Knit.CreateController {
+    Name = "DataController";
+    Reducers = {};
+}
+
+function DataController:Dispatch(actionTable)
+    if not self.Store then warn("Store not initialized") return end
+    self.Store:dispatch(actionTable)
+end
+
+function DataController:AddReducer(name, reducer)
+    if not self.Store then warn("Store not initialized") return end
+    self.Reducers[name] = reducer
+end
+
+function DataController:GetStore()
+    return self.Store
+end
+
+function DataController:KnitInit()
+    local dataService = Knit.GetService("DataService")
+
+    local function globalReducer(state,action)
+        state = state or {}
+        for _,reducer in pairs(self.Reducers) do
+            state = reducer(state,action)
+        end
+        return state
+    end
+
+    self:AddReducer("Replication",function(state,action)
+        if action.type == "syncState" then
+            state = action.newState
+        end
+        return state
+    end)
+
+    dataService.Action:Connect(function(action)
+        self:Dispatch(action)
+    end)
+
+    self.Store = Rodux.Store.new(globalReducer,{Inventory = {}, equipped = ""},{})
+end
+
+return DataController
---
+++ src/server/Services/DataService.lua
@@ -0,0 +1,50 @@
+--Rodux main service
+
+--API Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--Knit
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+--Dependencies
+local Rodux = require(ReplicatedStorage.Packages.Rodux)
+local ReplicationMiddleware = require(script.Parent.Parent.Modules.ReplicationMiddleware)
+
+--Service
+local DataService = Knit.CreateService {
+    Name = "DataService";
+    Reducers = {};
+    Client = {
+        Action = Knit:CreateSignal();
+    }
+}
+
+function DataService:Dispatch(actionTable)
+    if not self.Store then warn("Store not initialized") return end
+    self.Store:dispatch(actionTable)
+end
+
+function DataService:AddReducer(name: string, reducer: () -> {})
+    if not self.Store then warn("Store not initialized") return end
+    self.Reducers[name] = reducer
+end
+
+function DataService:GetStore()
+    return self.Store
+end
+
+function DataService:KnitInit()
+
+    local function globalReducer(state,action)
+        state = state or {}
+        for _,reducer in pairs(self.Reducers) do
+            state = reducer(state,action)
+        end
+        return state
+    end
+
+    self.Store = Rodux.Store.new(globalReducer,{},{ReplicationMiddleware(self.Client.Action)})
+
+end
+
+return DataService
